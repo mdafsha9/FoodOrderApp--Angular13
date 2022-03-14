9ff699c0e504e46ebe808422918e353c6497b1a5
mdafsha9 <mdafsha9@gmail.com>
2022-03-14 05:52:45 -0500

    changed images

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -6,4 +6,17 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
+  getAll():string[]{
+    return [
+      '/assets/pizza1.webp',
+      '/assets/burger1.webp',
+      '/assets/frenchfries.jpg',
+      '/assets/soup.jpg',
+      '/assets/pizza2.jpg',
+      '/assets/meetballs.jpg',
+      '/assets/pizza3.jpg',
+      '/assets/burger2.webp'
+
+    ]
+  }
 }
